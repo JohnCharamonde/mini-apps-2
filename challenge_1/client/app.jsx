9cac3843d8778adc707d5b525d7caffd531f6ebf
JohnCharamonde <john.charamonde@gmail.com>
2020-03-03 18:08:51 -0500

    Client set up, search component up and running, get request working for search and pagination

--- challenge_1/client/app.jsx
+++ challenge_1/client/app.jsx
@@ -1,2 +1,52 @@
+import React from 'react';
+import axios from 'axios';
+import Search from './Search.jsx';
 
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      q: '',
+      page: 1,
+      results: [],
+      search: ''
+    }
+  }
+
+  componentDidMount() {
+    axios.get(`http://localhost:3000/events?q=${this.state.q}&_page=${this.state.page}`)
+      .then(res => {
+        this.setState({
+          results: res.data
+        })
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+  handleSubmit(e) {
+    this.setState({
+      q: this.state.search,
+      page: 1
+    })
+  }
+
+  handleTyping(e) {
+    this.setState({
+      search: e.target.value
+    })
+  }
+
+
+  render() {
+    return (
+      <div>
+        <Search handleTyping={this.handleTyping.bind(this)} handleSubmit={this.handleSubmit.bind(this)}/>
+      </div>
+    )
+  }
+}
+
+export default App;
 
