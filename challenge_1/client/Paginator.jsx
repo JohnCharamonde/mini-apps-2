c984cdc5407e0792190e51997fbacfe15d40634d
JohnCharamonde <john.charamonde@gmail.com>
2020-03-03 20:37:01 -0500

    Paginator working with self-written inputs but formatting still off for headers next, first, last

--- challenge_1/client/Paginator.jsx
+++ challenge_1/client/Paginator.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 const Paginator = (props) => {
   return (
-    <div style={{display:"flex"}}>
-      <div>FIRST</div>
-      <div>PREVIOUS</div>
-      <div>CURRENT</div>
-      <div>NEXT</div>
-      <div>LAST</div>
+    <div style={{ display: "flex", width: "100%", alignItems: "center", justifyContent: "space-evenly", fontSize: "18px", fontFamily: "Helvetica", color:"#0000EE"}}>
+      <div onClick={(e) => {props.handlePaginatorClick('first', e)}}>Beggining of Time...</div>
+      <div onClick={(e) => {props.handlePaginatorClick('previous', e)}}>...Earlier...</div>
+      <div style={{color: "black"}}>Page: {props.page}</div>
+      <div onClick={(e) => {props.handlePaginatorClick('next', e)}}>...More Recent...</div>
+      <div onClick={(e) => {props.handlePaginatorClick('last', e)}}>...Present Day</div>
     </div>
   )
 }
--- challenge_1/client/app.jsx
+++ challenge_1/client/app.jsx
@@ -10,8 +10,11 @@ class App extends React.Component {
     this.state = {
       q: '',
       page: 1,
-      results: [{date:'', description:''}],
-      search: ''
+      results: [{ date: '', description: '' }],
+      search: '',
+      first: '',
+      next: '',
+      last: ''
     }
   }
 
@@ -19,7 +22,10 @@ class App extends React.Component {
     axios.get(`http://localhost:3000/events?q=${this.state.q}&_page=${this.state.page}`)
       .then(res => {
         this.setState({
-          results: res.data
+          results: res.data,
+          first: res.headers.link.split(',')[0].split(';')[0],
+          next: res.headers.link.split(',')[1].split(';')[0],
+          last: res.headers.link.split(',')[2].split(';')[0]
         })
       })
       .catch(err => {
@@ -32,7 +38,7 @@ class App extends React.Component {
       q: this.state.search,
       page: 1
     })
-    axios.get(`http://localhost:3000/events?q=${this.state.search}&_page=${this.state.page}`)
+    axios.get(`http://localhost:3000/events?q=${this.state.search}&_page=1`)
       .then(res => {
         this.setState({
           results: res.data
@@ -49,17 +55,50 @@ class App extends React.Component {
     })
   }
 
+  handlePaginatorClick(category, e) {
+    let uri;
+    if(category === 'first') {
+       uri = this.state.first;
+    } else if(category === 'previous') {
+      if(this.state.page > 1) {
+        uri = `http://localhost:3000/events?q=${this.state.q}&_page=${this.state.page - 1}`;
+      }
+    } else if(category === 'next') {
+      uri = this.state.next;
+    } else if(category === 'last') {
+      uri = this.state.last;
+    }
+
+    axios.get(uri)
+      .then(res => {
+        this.setState({
+          results: res.data,
+          first: res.headers.link.split(',')[0].split(';')[0],
+          next: res.headers.link.split(',')[1].split(';')[0],
+          last: res.headers.link.split(',')[2].split(';')[0]
+        })
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
   render() {
     console.log(this.state)
     return (
       <div>
-        <Search handleTyping={this.handleTyping.bind(this)} handleSubmit={this.handleSubmit.bind(this)}/>
-        <div style={{display: "flex"}}>
-        <div style={{display: "flex", width: "20%"}}></div>
-        <Results results={this.state.results}/>
-        <div style={{display: "flex", width: "20%"}}></div>
+        <Search handleTyping={this.handleTyping.bind(this)} handleSubmit={this.handleSubmit.bind(this)} />
+        <div style={{ display: "flex" }}>
+          <div style={{ display: "flex", width: "20%" }}></div>
+          <Results results={this.state.results} />
+          <div style={{ display: "flex", width: "20%" }}></div>
         </div>
-        <Paginator />
+        <div style={{ height: "40px" }}></div>
+        <div style={{ display: "flex" }}>
+          <div style={{ display: "flex", width: "20%" }}></div>
+          <Paginator page={this.state.page} handlePaginatorClick={this.handlePaginatorClick.bind(this)}/>
+          <div style={{ display: "flex", width: "20%" }}></div>
+        </div>
       </div>
     )
   }
