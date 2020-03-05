72d4f02880a80d95a31fc808f125f4ea686f22e3
JohnCharamonde <john.charamonde@gmail.com>
2020-03-05 14:15:07 -0500

    React component also up and running + slight syntactic updates to server.js

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,23 @@
+import React from 'react';
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+      this.state = {
 
+      }
+  }
+
+  componentDidMount(){
+    console.log('YUP')
+  }
+  render() {
+    return(
+    <div>
+      <div>OH YES!!!!!!!!!!!!!!!!</div>
+    </div>
+    )
+  }
+}
+
+export default App;
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -5,5 +5,5 @@ const PORT = 3800;
 
 app.use('/', express.static('./public'));
 
-app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
+app.listen(PORT, () => console.log(`Listening on port ${PORT}!`))
 
